4d74dabcb43909b55f100c27e1b6be4317145f1a
ejioformiracle1 <whitecheche51@gmail.com>
2026-07-01 14:00:50 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -402,5 +402,5 @@ class NigerianAccountBot:
 target_url = "https://nnnrc.com/#/register"
 NUM_ACCOUNTS = 5
 
-bot = NigerianAccountBot(start_code=201546)  # ← START FROM 0101621
+bot = NigerianAccountBot(start_code=201537)  # ← START FROM 0101621
 bot.run(target_url, num_accounts=NUM_ACCOUNTS)
